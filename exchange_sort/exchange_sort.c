1890406400aaf9b06505d633a9ced10430572ebf
kelr <kruan6818@gmail.com>
2018-01-18 21:41:42 -0500

    I believe it works

--- exchange_sort/exchange_sort.c
+++ exchange_sort/exchange_sort.c
@@ -1,14 +1,16 @@
-void exchange_sort(int input, size_t size)
+#include "exchange_sort.h"
+
+void exchange_sort(int *input, size_t size)
 {
     for (int i = 0; i < size; i++)
     {
-        for (int j = i + 1; j <= size; j++)
+        for (int j = i + 1; j < size; j++)
         {
             if (input[j] < input[i])
             {
                 int temp = input[i];
                 input[i] = input[j];
-                input[l] = temp;
+                input[j] = temp;
             }
         }
     }
@@ -16,11 +18,14 @@ void exchange_sort(int input, size_t size)
 
 int main(void)
 {
-    int snacks[10] = [5,2,8,34,8,2,4,89,-10,11];
-    size_t size = sizeof(snacks) / sizeof(snacks[0])
-    exchange_sort(snacks, size);
+    int snacks[10] = {5, 2, 8, 34, 8, 2, 4, 89, -10, 11};
+    size_t size = sizeof(snacks) / sizeof(snacks[0]);
+    int *p_snacks = snacks;
+
+    exchange_sort(p_snacks, size);
+
     for (int i = 0; i < size; i++)
     {
-        printf("%d\n", i)
+        printf("%d\n", snacks[i]);
     }
 }
--- exchange_sort/exchange_sort.h
+++ exchange_sort/exchange_sort.h
@@ -1,6 +1,9 @@
 #ifndef _KEL_EXCHANGE_SORT_
 #define _KEL_EXCHANGE_SORT_
 
-void exchange_sort(int input, size_t size);
+#include "stddef.h" //size_t
+#include "stdio.h" //printf
+
+void exchange_sort(int *input, size_t size);
 
 #endif //_KEL_EXCHANGE_SORT_
