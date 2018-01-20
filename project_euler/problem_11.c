762641ab428bbb7935a62e500e3b2fb661d683aa
kelr <kruan6818@gmail.com>
2018-01-20 00:28:42 -0500

    Got horizontal and vertical working properly

--- project_euler/problem_11.c
+++ project_euler/problem_11.c
@@ -38,123 +38,135 @@ int find_diag(const int input_grid[20][20]);
 // we only really need to run through it once
 int find_horiz(const int input_grid[20][20])
 {
-	int max = 0;
-	int current_product = 1;
-	for (int i = 0; i < 20; i++)
-	{
-	    for (int j = 0; j < 20-3; j++)
-	    {
-	    	for (int k = 0; k < 4; k++)
-	    	{
-	    		current_product *= input_grid[i][j+k];
-	   		}
-		    if (current_product > max)
-			{
-				max = current_product;
-			}
-			current_product = 1;
-	    }
-	}
-	return max;
+    int size = 20;
+    int max = 0;
+    int current_product = 1;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size-3; j++)
+        {
+            for (int k = 0; k < 4; k++)
+            {
+                current_product *= input_grid[i][j+k];
+               }
+            if (current_product > max)
+            {
+                max = current_product;
+            }
+            current_product = 1;
+        }
+    }
+    return max;
 }
 
 // Because vertical checking covers both up and down,
 // we only really need to run through it once
 int find_vert(const int input_grid[20][20])
 {
-	int max = 0;
-	int current_product = 1;
-	for (int i = 0; i < 20-3; i++)
-	{
-    	printf("\n");
-	    for (int j = 0; j < 20; j++)
-	    {
-	    	printf("\n");
-	    	for (int k = 0; k < 4; k++)
-	    	{
-				printf("%d, %d\n", i+k, j);
-	    		current_product *= input_grid[i+k][j];
-	   		}
-		    if (current_product > max)
-			{
-				max = current_product;
-			}
-			current_product = 1;
-		}
-	}
-	return max;
+    int size = 20;
+    int max = 0;
+    int current_product = 1;
+    for (int i = 0; i < size-3; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            for (int k = 0; k < 4; k++)
+            {
+                current_product *= input_grid[i+k][j];
+               }
+            if (current_product > max)
+            {
+                max = current_product;
+            }
+            current_product = 1;
+        }
+    }
+    return max;
 }
 
 // We do need to check both diagonals though
 int find_diag(const int input_grid[20][20])
 {
-	// Backslash 
-	int max = 0;
-	int current_product = 1;
-	for (int i = 0; i < 20-3; i++)
-	{
-		//printf("\n");
-		for (int j = 0; j < 4; j++)
-		{
-			//printf("%d, %d\n", j+i, j);
-	    	current_product *= input_grid[j][j+1];
-		    if (current_product > max)
-			{
-				max = current_product;
-			}
-			current_product = 1;
-		}
-	}
-	// Forward slash
-	int max_forward = 0;
-	int current_product_forward = 1;
-	for (int i = 20-1; i > 2; i--)
-	{
-    	current_product_forward *= input_grid[i][i];
-	    if (current_product_forward > max_forward)
-		{
-			max_forward = current_product_forward;
-		}
-		current_product_forward = 1;
-	}
+    int size = 20;
+    // Backslash 
+    int max_back = 0;
+    int curr_back = 1;
+    for (int i = 0; i < size-3; i++)
+    {
+        for (int j = 0; j < size-3; j++)
+        {
+            for (int k = 0; k < 4; k++)
+            {
+                curr_back *= input_grid[i+k][j+k];
+               }
 
-	return max;
+            if (curr_back > max_back)
+            {
+                max_back = curr_back;
+            }
+            curr_back = 1;
+        }
+    }
+    // Forward slash
+    int max_forward = 0;
+    int curr_forward = 1;
+    for (int i = 19; i < size-3; i++)
+    {
+        printf("\n");
+        for (int j = 0; j < size-3; j++)
+        {
+            printf("\n");
+            for (int k = 0; k < 4; k++)
+            {
+                printf("%d, %d\n", i+k, j+k);
+                curr_forward *= input_grid[i+k][j+k];
+               }
+
+            if (curr_forward > max_forward)
+            {
+                max_forward = curr_forward;
+            }
+            curr_forward = 1;
+        }
+    }
+
+    return max;
 }
 
 int main(void)
 {
-	const int input_grid[20][20] = {
-			{8, 2, 22, 97, 38, 15, 0, 4, 0, 75, 4, 5, 7, 78, 52, 12, 5, 77, 91, 8},
-			{49, 49, 99, 4, 17, 81, 18, 57, 6, 87, 17, 4, 98, 43, 69, 48, 4, 56, 62, 0},
-			{81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 4, 67, 53, 88, 3, 3, 49, 13, 36, 65},
-			{52, 7, 95, 23, 4, 6, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91},
-			{22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 4, 4, 28, 66, 33, 13, 8},
-			{24, 47, 32, 6, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 2, 35, 17, 12, 5},
-			{32, 98, 81, 28, 64, 23, 67, 1, 26, 38, 4, 67, 59, 54, 7, 66, 18, 38, 64, 7},
-			{67, 26, 2, 68, 2, 62, 12, 2, 95, 63, 94, 39, 63, 8, 4, 91, 66, 49, 94, 21},
-			{24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72},
-			{21, 36, 23, 9, 75, 0, 76, 44, 2, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95},
-			{78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 8, 4, 62, 16, 14, 9, 53, 56, 92},
-			{16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57},
-			{86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 6, 21, 58, 51, 54, 17, 58},
-			{19, 8, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 4},
-			{4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66},
-			{88, 36, 68, 87, 57, 62, 2, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69},
-			{4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 4, 62, 76, 36},
-			{2, 69, 36, 41, 72, 3, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16},
-			{2, 73, 35, 29, 78, 31, 9, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54},
-			{1, 7, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48}
-			};
+    const int input_grid[20][20] = {
+            {8, 2, 22, 97, 38, 15, 0, 4, 0, 75, 4, 5, 7, 78, 52, 12, 5, 77, 91, 8},
+            {49, 49, 99, 4, 17, 81, 18, 57, 6, 87, 17, 4, 98, 43, 69, 48, 4, 56, 62, 0},
+            {81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 4, 67, 53, 88, 3, 3, 49, 13, 36, 65},
+            {52, 7, 95, 23, 4, 6, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91},
+            {22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 4, 4, 28, 66, 33, 13, 8},
+            {24, 47, 32, 6, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 2, 35, 17, 12, 5},
+            {32, 98, 81, 28, 64, 23, 67, 1, 26, 38, 4, 67, 59, 54, 7, 66, 18, 38, 64, 7},
+            {67, 26, 2, 68, 2, 62, 12, 2, 95, 63, 94, 39, 63, 8, 4, 91, 66, 49, 94, 21},
+            {24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72},
+            {21, 36, 23, 9, 75, 0, 76, 44, 2, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95},
+            {78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 8, 4, 62, 16, 14, 9, 53, 56, 92},
+            {16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57},
+            {86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 6, 21, 58, 51, 54, 17, 58},
+            {19, 8, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 4},
+            {4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66},
+            {88, 36, 68, 87, 57, 62, 2, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69},
+            {4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 4, 62, 76, 36},
+            {2, 69, 36, 41, 72, 3, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16},
+            {2, 73, 35, 29, 78, 31, 9, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54},
+            {1, 7, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48}
+            };
 
-	clock_t start = clock();
-	int greatest_horiz = find_horiz(input_grid);
-	int greatest_vert = find_vert(input_grid);
-	int greatest_diag = find_diag(input_grid);
-	clock_t end = clock();
-	double total = (((double)(end - start)) / CLOCKS_PER_SEC)*1000;
-	printf("%d\n", greatest_horiz);
-	printf("%d\n", greatest_vert);
-	printf("%d\n", greatest_diag);
-	printf("Time taken: %f ms\n", total);
+    clock_t start = clock();
+    int greatest_horiz = find_horiz(input_grid);
+    int greatest_vert = find_vert(input_grid);
+    int greatest_diag = find_diag(input_grid);
+    clock_t end = clock();
+    double total = (((double)(end - start)) / CLOCKS_PER_SEC)*1000;
+    printf("%d\n", greatest_horiz);
+    printf("%d\n", greatest_vert);
+    printf("%d\n", greatest_diag);
+    printf("Time taken: %f ms\n", total);
     return 0;
 }
