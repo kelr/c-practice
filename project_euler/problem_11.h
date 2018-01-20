417912c6de0662a8b2f1d397a7025852245f4e13
kelr <kruan6818@gmail.com>
2018-01-19 23:36:15 -0500

    I cant initialize arrays with variables?

--- project_euler/problem_11.h
+++ project_euler/problem_11.h
@@ -1,6 +1,12 @@
 #ifndef _KEL_EULER_11_H_
 #define _KEL_EULER_11_H_
 
+#include "stdio.h" //printf()
+typedef struct
+{
+    int i;
+} grid;
 
+int find_greatest_product(grid *input_grid, unsigned int size);
 
 #endif //_KEL_EULER_11_H_
