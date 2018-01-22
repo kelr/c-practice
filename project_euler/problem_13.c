128de9194ca0d2651a62901af0d551f0bde246a5
kelr <kruan6818@gmail.com>
2018-01-21 23:54:10 -0500

    Progress

--- project_euler/problem_13.c
+++ project_euler/problem_13.c
@@ -4,35 +4,49 @@ Work out the first ten digits of the sum of the following one-hundred 50-digit n
 -- Number is in file problem_13_input
 */
 #include "stdio.h" //printf()
-#include "math.h" //sqrt(), make sure to compile with the -lm flag
-#include "time.h" //clock(), CLOCKS_PER_SEC, clock_t
+#include "stdlib.h" //getline()
 
+void sum_big_numbers(char* num1, char* num2);
+
+// Sum big numbers and store the result in num1.
+void sum_big_numbers(char* num1, char* num2)
+{
+    int current_num_1 = 0;
+    int current_num_2 = 0;
+    int temp_sum = 0;
+    int carry_over = 0;
+    for (int i = 49; i > -1; i--)
+    {
+        // Convert the chars to ints for this unit place
+        current_num_1 = num1[i] - '0';
+        current_num_2 = num2[i] - '0';
+        temp_sum = current_num_2 + current_num_1;
+        if (temp_sum >= 10) carry_over = temp_sum - 10;
+        printf("%d, %d\n", temp_sum, carry_over);
+    }
+}
 
 
 int main(int argc, char *argv[])
 {
-    unsigned int nth_triangle_num = 0;
-    unsigned int curr_triangle_num = 0;
-    unsigned int count = 0;
-    unsigned int max_count = 0;
-    clock_t start = clock();
-    while (max_count <= 500)
+    FILE *f;
+    char *curr_line = NULL;
+    //char *curr_sum = "00000000000000000000000000000000000000000000000000";
+    char *curr_sum = "46376937677490009712648124896970078050417018260538";
+    size_t len = 0;
+    ssize_t num_chars_read;
+
+    f = fopen("problem_13_input", "r");
+    if (f == NULL) return 1;
+
+    while ((num_chars_read = getline(&curr_line, &len, f)) != -1) 
     {
-        // Build triangle numbers by adding the desired position to the previous one
-        nth_triangle_num++;
-        curr_triangle_num += nth_triangle_num;
-        count = count_factors(curr_triangle_num);
-        if (count > max_count)
-        {
-            max_count = count;
-            printf("%d\n", max_count);
-        }
-
+        sum_big_numbers(curr_sum, curr_line);
+        break;
     }
-    clock_t end = clock();
-    double total_s = (((double)(end - start)) / CLOCKS_PER_SEC);
 
-    printf("The %dth Triangle num: %d has %d factors\n",nth_triangle_num, curr_triangle_num, max_count);
-    printf("Time taken: %f s\n", total_s);
+    fclose(f);
+    if (curr_line) free(curr_line);
+
     return 0;
 }
