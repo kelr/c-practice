e45f7f1ea0192a7579474bc148aa7920c9d61b6c
kelr <kruan6818@gmail.com>
2018-01-31 21:41:46 -0500

    Gotta close those file descriptors

--- classwork_operating_systems/3-IPC/pipe_extra_cred.c
+++ classwork_operating_systems/3-IPC/pipe_extra_cred.c
@@ -36,7 +36,7 @@ int main(void)
     }
 
     // Child one block
-    if (pid_fork_1 == 0 ) 
+    if (pid_fork_1 == 0) 
     {
         // Close child one's stdout (fd = 1)
         close(1); 
@@ -47,9 +47,13 @@ int main(void)
         // Close the read fd of pipe 1, because we don't need it
         close(fd_pipe_1[0]);
 
+        // Close pipe 2, we don't need it at all
+        close(fd_pipe_2[0]);
+        close(fd_pipe_2[1]);
+
         // Load the ps binary and execute ps -elf with the exec system call
         // Stdout of this command will be sent to the write fd of pipe 1
-        if (execlp("ps", "ps", "-elf", (char *) 0) < 0) 
+        if (execlp("ps", "ps", "-elf", NULL) < 0)
         {
             perror("Error in executing command 'ps -elf' in child 1");
             exit(0);
@@ -71,7 +75,7 @@ int main(void)
             close(0);
 
             // Duplicate the read fd of pipe 1, make it stdin
-            dup2(fd_pipe_1[0],0);  
+            dup2(fd_pipe_1[0], 0);
 
             // Close the write fd of pipe 1, we don't need it
             close(fd_pipe_1[1]);
@@ -88,7 +92,7 @@ int main(void)
             // Load the grep binary and execute grep. 
             // Stdin of this command will be the read fd of pipe 1
             // Stdout of thie command will be the write fd of pipe 2
-            if( execlp("grep", "grep", "/usr", (char *) 0) < 0) 
+            if (execlp("grep", "grep", "/usr", NULL) < 0)
             {
                 perror("Error in executing command 'grep \"/usr\"' in child 2");
                 exit(0);
@@ -104,20 +108,24 @@ int main(void)
                 exit(1);
             }
 
-            if (pid_fork_3 == 0 ) 
+            if (pid_fork_3 == 0) 
             {
                 // Close child three's stdin (fd = 0)
                 close(0);   
 
                 // Duplicate the read fd of pipe 2, make it stdin
-                dup2(fd_pipe_2[0],0);  
+                dup2(fd_pipe_2[0], 0);  
 
                 // Close the write fd of pipe 2, we don't need it
                 close(fd_pipe_2[1]);
 
+                // Close pipe 1, we don't need it at all
+                close(fd_pipe_1[0]);
+                close(fd_pipe_1[1]);
+
                 // Load the wc binary and execute wc. 
                 // Stdin of this command will be receieved by the read fd of pipe 2
-                if( execlp("wc", "wc", "-l", (char *) 0) < 0) 
+                if (execlp("wc", "wc", "-l", NULL) < 0) 
                 {
                     perror("Error in executing command 'wc -l' in child 3");
                     exit(0);
