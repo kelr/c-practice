c365caaf07dbcf95687c8465b1e7862afa4993ba
kelr <kruan6818@gmail.com>
2018-02-10 23:02:17 -0500

    Working on mergesort

--- classwork_algorithms/merge_sort/merge_sort.c
+++ classwork_algorithms/merge_sort/merge_sort.c
@@ -1,6 +1,27 @@
-#include "merge_sort.h"
 
-void merge_sort(int *input, size_t size)
+void merge(const int* input_one, const int* input_two, size_t size_one, size_t size_two, int* output)
+void merge_sort(int* input, size_t size)
+
+void merge(const int* input_one, const int* input_two, size_t size_one, size_t size_two, int* output)
 {
 
+}
+
+void merge_sort(int* input, size_t size)
+{
+
+    if (size > 1)
+    {
+        // Split the input array into two and determine their sizes
+        const size_t size_one = floor(size / 2);
+        const size_t size_two = size - end_index_one;
+
+        int* array_one = malloc(sizeof(size_t) * size_one);
+        int* array_two = malloc(sizeof(size_t) * size_two);
+
+        
+
+    }
+
+    return input;
 }
