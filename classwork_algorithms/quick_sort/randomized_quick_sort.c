b57bc2f0d9ef96ef141f63243e4a0cab21f80500
kelr <kruan6818@gmail.com>
2018-02-11 22:06:30 -0500

    Quicksort

--- classwork_algorithms/quick_sort/randomized_quick_sort.c
+++ classwork_algorithms/quick_sort/randomized_quick_sort.c
@@ -1,6 +1,22 @@
-#include "rand_quick_sort.h"
+
+void partition(int low, int high, int pivot);
+void rand_quick_sort(int *input, size_t size);
+
+void partition(int low, int high, int pivot)
+{
+
+}
 
 void rand_quick_sort(int *input, size_t size)
 {
+	if (size > 1)
+	{
 
+	}
+	return input;
+}
+
+int main(void)
+{
+	return 0;
 }
