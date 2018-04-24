e91b0380ca3abcd836ea600bc5e30202f9e57de9
kelr <kruan6818@gmail.com>
2018-04-24 02:05:14 -0400

    Make stuff

---
+++ classwork_operating_systems/4-threads/thread_extra_cred.c
@@ -0,0 +1,11 @@
+#include <stdio.h> //perror(), printf()
+#include <stdlib.h> //exit(), 
+#include <sys/types.h> //pid_t, fork()
+#include <sys/wait.h> //wait()
+#include <unistd.h> //close(), dup2(), execlp(), fork()
+
+
+int main(void)
+{
+    return 0;
+}
